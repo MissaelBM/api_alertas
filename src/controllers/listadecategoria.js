15f8243996f1dc48f40360e7b44d41935e2fdb7a
missael <alebermor999@gmail.com>
2025-02-12 14:25:15 -0600

    Lista de categoria (ruta y controlador: consultar, consultar individual, insertar, actualizar y eliminar)

---
+++ src/controllers/listadecategoria.js
@@ -0,0 +1,108 @@
+module.exports = (connection) => {
+    return {
+        consultar: async (req, res) => {
+            try {
+              const [rows] = await connection.promise().query('SELECT * FROM listadecategoria WHERE eliminado = ?', [false]);
+              res.status(200).json(rows);
+            } catch (error) {
+              console.error('Error:', error);
+              res.status(500).json({ message: 'Error' });
+            }
+      
+          },
+          consultarId: async (req, res) => {
+            const { idlistadecategoria } = req.params;
+      
+            try {
+              const [rows] = await connection.promise().query('SELECT * FROM listadecategoria WHERE idlistadecategoria = ? AND eliminado = ?', [idlistadecategoria, false]);
+      
+              if (rows.length === 0) {
+                return res.status(404).json({ message: 'Lista de categoria no encontrada' });
+              }
+      
+              res.status(200).json(rows[0]);
+            } catch (error) {
+              console.error('Error:', error);
+              res.status(500).json({ message: 'Error' });
+            }
+          },
+          listadecategoria: async (req, res) => {
+            const {cliente_idcliente, categoria_idcategoria } = req.body;
+      
+            try {
+      
+              const [result] = await connection.promise().query(
+                'INSERT INTO listadecategoria (cliente_idcliente, categoria_idcategoria, eliminado) VALUES (?, ?, ?)',
+                [cliente_idcliente, categoria_idcategoria, false]
+              );
+      
+              res.status(201).json({ message: 'Lista de categoria registrada', guardadoId: result.insertId });
+            } catch (error) {
+              console.error('Error al registrar lista de categoria:', error);
+              res.status(500).json({ message: 'Error al registrar Lista de categoria' });
+            }
+          },
+          actualizarListadecategoria: async (req, res) => {
+            const { idlistadecategoria } = req.params;
+            const {cliente_idcliente, categoria_idcategoria} = req.body;
+      
+            try {
+              let query = 'UPDATE listadecategoria SET ';
+              const updates = [];
+              const params = [];
+      
+             
+              if (cliente_idcliente) {
+                updates.push('cliente_idcliente = ?');
+                params.push(cliente_idcliente);
+              }
+      
+              if (categoria_idcategoria) {
+                updates.push('categoria_idcategoria = ?');
+                params.push(categoria_idcategoria);
+              }
+      
+             
+              if (updates.length === 0) {
+                return res.status(400).json({ message: 'Sin información' });
+              }
+      
+              query += updates.join(', ') + ' WHERE idlistadecategoria  = ?';
+              params.push(idlistadecategoria );
+      
+              const [result] = await connection.promise().query(query, params);
+      
+              if (result.affectedRows === 0) {
+                return res.status(404).json({ message: 'listadecategoria  no econtrada' });
+              }
+      
+              res.status(200).json({ message: 'listadecategoria  actualizada exitosamente' });
+            } catch (error) {
+              console.error('Error:', error);
+              res.status(500).json({ message: 'Error' });
+            }
+          },
+
+          eliminarListadecategoria : async (req, res) => {
+              const { idlistadecategoria } = req.params;
+    
+              try {
+                  
+                  const [result] = await connection.promise().query(
+                      'UPDATE listadecategoria SET eliminado = ? WHERE idlistadecategoria = ?',
+                      [true, idlistadecategoria]
+                  );
+    
+                  if (result.affectedRows === 0) {
+                      return res.status(404).json({ message: 'listadecategoria no encontrada' });
+                  }
+    
+                  res.status(200).json({ message: 'listadecategoria eliminada' });
+              } catch (error) {
+                  console.error('Error:', error);
+                  res.status(500).json({ message: 'Error' });
+              }
+          }
+
+    };
+};
