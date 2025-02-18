7c24fd2e96800179d74499ba2567a07eff2af43b
estefania <fanny27am@gmail.com>
2025-02-17 18:46:46 -0600

    cliente

--- src/controllers/cliente.js
+++ src/controllers/cliente.js
@@ -32,59 +32,65 @@ module.exports = (connection) => {
 
     // Crear un nuevo cliente
     crearCliente: async (req, res) => {
-      const {usuario_idusuario, nombre, telefono, ubicacion, eliminado } = req.body;
-
+      const { usuario_idusuario, nombre, telefono, ubicacion, eliminado } = req.body;
+    
+      // Construir la ubicación en formato 'POINT(x y)'
+      const ubicacionStr = `POINT(${ubicacion.x} ${ubicacion.y})`;
+    
       try {     
         const [result] = await connection.promise().query(
           'INSERT INTO cliente (usuario_idusuario, nombre, telefono, ubicacion, eliminado) VALUES (?, ?, ?, ST_GeomFromText(?), ?)',
-          [usuario_idusuario, nombre, telefono, ubicacion, eliminado]
+          [usuario_idusuario, nombre, telefono, ubicacionStr, eliminado]
         );
-
-        res.status(201).json({ message: 'Cliente registrado', clienteId: results.insertId });
+    
+        res.status(201).json({ message: 'Cliente registrado', clienteId: result.insertId });
       } catch (error) {
         console.error('Error al registrar cliente:', error);
         res.status(500).json({ message: 'Error al registrar cliente' });
       }
     },
     
+    
     // Actualizar un cliente por ID
     actualizarClientePorId: async (req, res) => {
       const { id } = req.params;
-      const {nombre, telefono, ubicacion} = req.body;
-
+      const { nombre, telefono, ubicacion } = req.body;
+    
       try {
         let query = 'UPDATE cliente SET ';
         const updates = [];
         const params = [];
-
+    
         if (nombre) {
           updates.push('nombre = ?');
           params.push(nombre);
         }
-
+    
         if (telefono) {
           updates.push('telefono = ?');
           params.push(telefono);
         }
-
+    
         if (ubicacion) {
-          updates.push('ubicacion = ?');
-          params.push(ubicacion);
+          // Convertir ubicación a formato 'POINT(x y)'
+          const ubicacionStr = `POINT(${ubicacion.x} ${ubicacion.y})`;
+          updates.push('ubicacion = ST_GeomFromText(?)');
+          params.push(ubicacionStr);
         }
-
+    
         if (updates.length === 0) {
           return res.status(400).json({ message: 'Sin información' });
         }
-
+    
         query += updates.join(', ') + ' WHERE idcliente = ?';
         params.push(id);
-
+    
         const [result] = await connection.promise().query(query, params);
-
+    
         if (result.affectedRows === 0) {
-          return res.status(404).json({ message: 'Cliente no econtrado' });
+          return res.status(404).json({ message: 'Cliente no encontrado' });
         }
-
+    
         res.status(200).json({ message: 'Cliente actualizado exitosamente' });
       } catch (error) {
         console.error('Error:', error);
