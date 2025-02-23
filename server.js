0f557634230573fc8d53a2e00c2a7081542d5952
missael <alebermor999@gmail.com>
2025-02-22 23:12:33 -0600

    Corrección servidor passwors

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const connection = mysql.createConnection({
     host: '127.0.0.1',
     port: 3306,
     user: 'root',
-    password: 'pokoo',
+    password: 'Fidel12-',
     database: 'alertas'
 });
 
