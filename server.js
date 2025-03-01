6dfe97d442fd7df25f26080dd3a9e694cde8c53f
missael <alebermor999@gmail.com>
2025-02-28 19:33:54 -0600

    Agregar puerto para db en env 8080

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const tarjetaRoutes = require('./src/routes/tarjeta');
 const listadecategoriaRoutes = require('./src/routes/listadecategoria');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 
 app.use(bodyParser.json());
